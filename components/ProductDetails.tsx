a12c01b498a70dca379706c4ee4ba11e236e9af4
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-08-08 14:22:19 +0200

    Add next Image components

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export interface ProductData {
     id: number
     title: string,
@@ -15,7 +17,9 @@ export const ProductDetails = ({data}: ProductProps) => {
     return (
         <div className="flex flex-col gap-4 p-4 border-blue-300 border-2 rounded-md">
             <span className="text-xl font-bold">{data.title}</span>
-            <img src={data.imageSrc} alt={data.imageAlt}/>
+            <div className="relative w-full h-[500px] bg-white">
+                <Image src={data.imageSrc} alt={data.imageAlt} fill className="object-contain" sizes="500px" priority/>
+            </div>
             <span>{data.description}</span>
             <span className="text-blue-800 font-bold">{data.rating}</span>
         </div>)
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,5 +1,6 @@
 import {ProductData} from "@/components/ProductDetails";
 import Link from "next/link";
+import Image from "next/image";
 
 export type ProductListItemData = Pick<ProductData, 'id' | 'title' | 'imageSrc' | 'imageAlt'>
 
@@ -9,8 +10,11 @@ interface Props {
 
 export const ProductListItem = ({data}: Props) => {
     return (
-        <Link href={`/products/${data.id}`} className="flex flex-col gap-4 p-4 border-blue-300 border-2 rounded-md">
+        <Link href={`/products/${data.id}`}
+              className="flex flex-col justify-between gap-4 p-4 border-blue-300 border-2 rounded-md">
             <span className="text-xl font-bold">{data.title}</span>
-            <img src={data.imageSrc} alt={data.imageAlt}/>
+            <div className="relative w-full h-[300px] bg-white">
+                <Image src={data.imageSrc} alt={data.imageAlt} fill className="object-contain" sizes="300px" priority/>
+            </div>
         </Link>)
 }
