777903d9fc8460b0db4bcda69325f12202675963
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-08-09 14:50:54 +0200

    Add basic SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,17 @@
+import {NextSeoProps} from "next-seo";
+
+const title = "Zai-Shop"
+const description = "The best shop ever."
+
+const NextSeoConfig: NextSeoProps = {
+    title,
+    description,
+    openGraph: {
+        url: 'https://zai-shop.vercel.app/',
+        title,
+        description,
+        siteName: title,
+    },
+}
+
+export default NextSeoConfig
