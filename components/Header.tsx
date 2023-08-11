d4438dea6afca6e7a0f87741a7e52052493644cf
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-08-11 10:42:45 +0200

    Add CartBar to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {CartBar} from "@/components/Cart/CartBar";
 
 export const Header = () => {
     const {pathname} = useRouter()
 
-    return <header className="bg-gray-700 px-4 py-2">
+    return <header className="flex justify-between bg-gray-700 px-4 py-2">
         <nav className="flex flex-row gap-4  text-white">
             <Link className={pathname === '/' ? "font-bold" : ''} href="/">
                 Home
@@ -16,5 +17,6 @@ export const Header = () => {
                 Products
             </Link>
         </nav>
+        <CartBar/>
     </header>
 }
