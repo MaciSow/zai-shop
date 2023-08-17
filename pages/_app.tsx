4aba575e6f33d6a21717389cb58a111d5b5f88a0
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-08-17 08:59:58 +0200

    Add graphql  & apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,18 @@ import {Layout} from "@/components/Layout";
 import {DefaultSeo} from "next-seo";
 import SeoConfig from "@/next-seo.config"
 import {CartContextProvider} from "@/store/CartContext";
+import {ApolloProvider} from "@apollo/client";
+import {apolloClient} from "@/graphql/apolloClient";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <CartContextProvider>
-            <Layout>
-                <DefaultSeo {...SeoConfig}/>
-                <Component {...pageProps} />
-            </Layout>
-        </CartContextProvider>
+        <ApolloProvider client={apolloClient}>
+            <CartContextProvider>
+                <Layout>
+                    <DefaultSeo {...SeoConfig}/>
+                    <Component {...pageProps} />
+                </Layout>
+            </CartContextProvider>
+        </ApolloProvider>
     )
 }
